bb3674419b8509379c2b5a656b4229798a4bbd0c
Paddy McGlone <paddymcglone@gmail.com>
2019-04-29 19:38:57 +0100

    Adding first commit.

---
+++ DevOpsTestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace DevOpsTestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var message = Greeting();
+            Console.WriteLine(message);
+        }
+
+        static string Greeting()
+        {
+            var currentHour = DateTime.Now.Hour;
+
+            if (currentHour < 12)
+                return "Good Morning";
+
+            if (currentHour > 12 && currentHour < 20)
+                return "Good Afternoon";
+
+            return "Good Evening";
+        }
+    }
+}
